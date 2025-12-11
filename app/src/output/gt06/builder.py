69b9320ec3f553e4f95f93a46e4f018a385348e9
Juan Bispo <juan.bisposantos05@gmail.com>
2025-12-11 13:39:44 -0300

    Documenting the "build_location_packet" function...

--- app/src/output/gt06/builder.py
+++ app/src/output/gt06/builder.py
@@ -14,12 +14,22 @@ def build_location_packet(dev_id: str, packet_data: dict, serial_number: int, *a
     Suports different types of protocols (protocol_number): 0x22, 0x32, 0xA0 
     """
 
+    # Note on struct: struct is a python library used to pack python native types (ex: int, string, float, etc...)
+    # Into a binary data, or to unpack binary data into python native types.
+    # The methods struct.pack and struct.unpack needs the developer to specify the format in the first argument
+    # ">" Means "Big Endian" it's the "orientation" of the number, so how we read "1000" from left to right
+    # the binary data must be read this way (in other hand "<" means "Little Endian" wich is the opposite)
+    # "B" means "Byte" or "One Byte" it tells struct to pack python data into a single byte or to unpack a single byte to a python data
+    # "H" means "Word" or "Two bytes" and follows the same logic, so how are "I" and "Q".
+
     protocol_number = settings.GT06_LOCATION_PACKET_PROTOCOL_NUMBER # We will use the protocol_number specified in settings
-    
+
+    # First we get the timestamp from the packet data
+    # and dissecate it part-by-part, pass it to struct and pack into "time_bytes"
     timestamp: datetime = packet_data.get("timestamp", datetime.now())
     time_bytes = struct.pack(
-        ">BBBBBB",
-        timestamp.year % 100,
+        ">BBBBBB", # We pass this format, that means one byte for each part of the date, in ">" Big Endian
+        timestamp.year % 100, # Module of the year, gets only the final part. Ex: 2025 -> 25
         timestamp.month,
         timestamp.day,
         timestamp.hour,
@@ -27,31 +37,52 @@ def build_location_packet(dev_id: str, packet_data: dict, serial_number: int, *a
         timestamp.second,
     )
 
+    # Now we will mount the gps length and quantity of satellites byte
+    # this byte combine two informations, the gps information length that is fixed to 12 (0xC in Hex)
+    # And the quantity of satellites
     packet_satellites = packet_data.get("satellites", 0)
-    satellites = min(15, packet_satellites) 
-    gps_info_byte = 0xC0 | satellites
+    satellites = min(15, packet_satellites) # The quantity of satellites field in the GT06 protocol have a fixed length of a nibble (4 bits, max decimal value of 15)
+    gps_info = 0xC0 | satellites # Now we apply a mask (OR) to merge the two informations into a single byte
+    gps_info_byte = struct.pack(">B", gps_info)
 
+    # Mounting the latitude and longitue bytes
     latitude_val = packet_data.get("latitude", 0.0)
     longitude_val = packet_data.get("longitude", 0.0)
     
+    # converting the floating point values of lat and long into integers
     lat_raw = int(abs(latitude_val) * 1800000)
     lon_raw = int(abs(longitude_val) * 1800000)
+
+    # Packing it into "lat_lon_bytes"
     lat_lon_bytes = struct.pack(">II", lat_raw, lon_raw)
 
+    # Mouting the speed kmh byte
     speed_kmh = int(packet_data.get("speed_kmh", 0))
     speed_kmh_bytes = struct.pack(">B", speed_kmh)
 
+    # Mouting the course and status byte
+    # Course and status byte carryes various informations
+    # The principals informations are: GPS Fixed, Hemisfer of latitude and lingitude
+    # And the direction of the movement 
+
+    # Here we apply a mask that isolates the 11 last significant bytes (the ones to the rigth)
+    # To have the certainty that this field only ocupates 11 bits length
     direction = int(packet_data.get("direction", 0)) & 0x03FF
+
+    # Then we prepare the other information tu put in the five bits left
     gps_fixed = 1 if packet_data.get("gps_fixed", False) else 0
     
-    is_latitude_north = 1 if latitude_val >= 0 else 0
-    is_longitude_west = 1 if longitude_val < 0 else 0
+    is_latitude_north = 1 if latitude_val >= 0 else 0 # Deciding the hemisfer of latitude
+    is_longitude_west = 1 if longitude_val < 0 else 0 # Deciding the hemisfer of longitude
 
+    # Here we use a mask (OR) to combine the direction byte with the others informations packing it to a 2 bytes length field
     course_status = (gps_fixed << 12) | (is_longitude_west << 11) | (is_latitude_north << 10) | direction
-    course_status_bytes = struct.pack(">H", course_status)
+    course_status_bytes = struct.pack(">H", course_status) # Packing it
     
-    content_body = time_bytes + struct.pack(">B", gps_info_byte) + lat_lon_bytes + speed_kmh_bytes + course_status_bytes
-
+    # ==============================================================================================================================
+    # Finally putting it all together, forming the body of the packet
+    content_body = time_bytes + gps_info_byte + lat_lon_bytes + speed_kmh_bytes + course_status_bytes
+    
     acc_status = 1 if packet_data.get("acc_status", 1) else 0
     gps_odometer = int(packet_data.get("gps_odometer", 0))
     voltage = float(packet_data.get("voltage", 0.0))
