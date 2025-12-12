70446f2a3974f85e37562ed9b78364387b1a4027
Juan Bispo <juan.bisposantos05@gmail.com>
2025-12-12 18:16:57 -0300

    Creating a basic model to organize and strucuture to dinamically choose between different output packet builders according to the given output protocol.

---
+++ app/src/output/output_mappers.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+from . import gt06, suntech4g
+
+class OutputMappers(BaseModel):
+    
+    OUTPUT_PACKET_BUILDERS: dict = {
+        "gt06": {
+            "location": gt06.build_location_packet,
+            "heartbeat": gt06.build_heartbeat_packet,
+            "info": gt06.build_voltage_info_packet,
+            "login": gt06.build_login_packet,
+        },
+        "suntech4g": {
+            "location": suntech4g.build_location_packet,
+            "heartbeat": suntech4g.build_heartbeat_packet,
+            "login": suntech4g.build_login_packet,
+        }
+    }
+
+    OUTPUT_COMMAND_MAPPERS: dict = {
+        "gt06": gt06.map_command,
+        "suntech4g": suntech4g.map_command,
+    }
+
+output_mappers = OutputMappers()
