d709fef0423025a4cc80a96a5dbfdc3817436cd8
Juan Bispo <juan.bisposantos05@gmail.com>
2025-12-12 11:51:28 -0300

    For each location we check if this location have already been processed, and if its not, we forwards it to the processor of the mt02 input source. Here were using threads to not block the main loop of the worker, in case of many devices retrieved from "api_client.fetch_all()".

--- app/src/input/mt02/worker.py
+++ app/src/input/mt02/worker.py
@@ -1,8 +1,13 @@
+import threading
+
+from . import processor
 from .api_client import MT02ApiClient
 from app.config.settings import settings
+from app.services.redis_service import get_redis
 from app.core.logger import get_logger
 
 logger = get_logger(__name__)
+redis_client = get_redis()
 
 api_client = MT02ApiClient(api_key=settings.MT02_API_KEY)
 
@@ -21,6 +26,24 @@ def worker():
                 continue
 
             logger.info(f"Fetched locations for {len(locations)} devices.")
+
+            # For each location, we check if the data was already processed in a past iteration.
+            for device_id, location in locations.items():
+                device_key = f"device:mt02:{device_id}"
+                last_processed_timestamp = redis_client.hget(device_key, "last_timestamp")
+
+                # If the location data is new (not processed before), we store it in Redis.
+                is_new = not last_processed_timestamp or int(location['timestamp']) > int(last_processed_timestamp)
+                if not is_new:
+                    logger.info(f"No new location for device {device_id}.")
+                    continue
+                
+                # Store the new location timestamp in Redis to mark it as processed.
+                redis_client.hset(device_key, "last_timestamp", location["timestamp"])
+                logger.info(f"New location for device {device_id}: {location}")
+
+                # Pass the new location to the processor for further handling.
+                threading.Thread(target=processor.process_location, args=(device_id, location)).start()
 
         except Exception as e:
             logger.info(f"Error in worker loop: {e}")
