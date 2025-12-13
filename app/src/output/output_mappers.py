cba486ccd4996eb21934840787f427644e9e5bd1
Juan Bispo <juan.bisposantos05@gmail.com>
2025-12-13 14:06:59 -0300

    Omiting the declaration of the suntech4g builder functions as they are not defined yet

--- app/src/output/output_mappers.py
+++ app/src/output/output_mappers.py
@@ -11,16 +11,16 @@ class OutputMappers(BaseModel):
             "info": gt06.build_voltage_info_packet,
             "login": gt06.build_login_packet,
         },
-        "suntech4g": {
-            "location": suntech4g.build_location_packet,
-            "heartbeat": suntech4g.build_heartbeat_packet,
-            "login": suntech4g.build_login_packet,
-        }
+        # "suntech4g": {
+        #     "location": suntech4g.build_location_packet,
+        #     "heartbeat": suntech4g.build_heartbeat_packet,
+        #     "login": suntech4g.build_login_packet,
+        # }
     }
 
     OUTPUT_COMMAND_MAPPERS: dict = {
         "gt06": gt06.map_command,
-        "suntech4g": suntech4g.map_command,
+        # "suntech4g": suntech4g.map_command,
     }
 
 output_mappers = OutputMappers()
