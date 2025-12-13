a4980bcbfb24ac020202adafba29e6ad4eaedd6b
Juan Bispo <juan.bisposantos05@gmail.com>
2025-12-13 14:35:12 -0300

    Developing a odometer calculating logic, by using redis to persist the state of the device and haversine to calculate the distances between two coordinate points

--- app/src/input/mt02/mapper.py
+++ app/src/input/mt02/mapper.py
@@ -2,8 +2,11 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 from app.core.logger import get_logger
+from app.services.redis_service import get_redis
+from .. import utils as input_source_utils
 
 logger = get_logger(__name__)
+redis_client = get_redis()
 
 def map_location_data(device_id: str, location: dict) -> dict:
     """
@@ -29,11 +32,35 @@ def map_location_data(device_id: str, location: dict) -> dict:
     # Then adding the hours
     date_time += relativedelta(hours=3)
 
+    lat, lon = location.get("lat"), location.get("lng")
+    if not lat or not lon:
+        logger.error(f"It was not possible to continue the mapping of the data, coordinates corrupted.")
+        return {}
+    
+    device_key = device_key = f"device:mt02:{device_id}"
+
+    odometer = redis_client.hget(device_key, "last_odometer")
+    if not odometer:
+        odometer = 0
+
+    last_lat, last_lon = redis_client.hmget(device_key, "last_lat", "last_lon")
+    if last_lat and last_lon:
+        args = list(map(float, [lat, lon, last_lat, last_lon]))
+
+        meters_calculated = input_source_utils.haversine(*args)
+        odometer += meters_calculated
+
+        redis_client.hset(device_key, "last_odometer", odometer)
+
+    else:
+        logger.warning(f"There are not coordinates stored in the devices state storage. Continue with 0 odometer")
+        
     mapped_data = {
         "timestamp": date_time,
-        "latitude": location.get("lat"),
-        "longitude": location.get("lng"),
-        "satellites": 6 # Mock satellites so the server can accept our packets
+        "latitude": lat,
+        "longitude": lon,
+        "satellites": 6, # Mock satellites so the server can accept our packets
+        "gps_odometer": odometer,
     }
     
     return mapped_data
