fdcd4d98b6cd9dd2180bbceab0f028add7818901
Juan Bispo <juan.bisposantos05@gmail.com>
2025-12-13 13:54:28 -0300

    First design of the main function in the project. Its inside "main" module, and has the intent of start any workers, listeners, and all the processes and ports needed for the well working of the software

---
+++ main.py
@@ -0,0 +1,36 @@
+import importlib
+import threading
+import dotenv
+dotenv.load_dotenv()
+
+from app.config.settings import settings
+from app.core.logger import get_logger
+
+logger = get_logger(__name__)
+
+def main():
+    """
+    Docstring for main
+    """
+
+    with logger.contextualize(log_label="SERVER"):
+        for input_source, worker_location in settings.WORKERS_INPUT_SOURCE.items():
+            module_path = worker_location.get("module_path")
+            func_name = worker_location.get("func_name")
+
+            logger.info(f"Initiating worker for the input source: {input_source}. Worker location: {module_path}.{func_name}")
+
+            target_module = importlib.import_module(module_path)
+            if not target_module:
+                logger.error(f"There was an error importing the module {module_path} for the input source {input_source} to initialize the worker.")
+                continue
+
+            target_func = getattr(target_module, func_name)
+            if not target_func:
+                logger.error(f"Worker function not defined in {module_path}. Please verify.")
+                continue
+
+            threading.Thread(target=target_func, daemon=True).start()
+
+if __name__ == "__main__":
+    main()
